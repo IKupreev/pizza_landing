71bdc0160008bd1e40fadea639817f6a6f72cb95
IKupreev <igor.kupreev88@gmail.com>
2022-06-26 23:16:14 +0700

    add routing, home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { PromoComponent } from './components/promo/promo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: '/home', pathMatch: 'full'},
+  {path:'home', component: HomeComponent},
+  {path:'menu', component: MenuComponent},
+  {path:'promo', component: PromoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
