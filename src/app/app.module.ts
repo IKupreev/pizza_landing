20a9c9c9694e7ff0b44d2dbad000c5ff55f52e1b
IKupreev <igor.kupreev88@gmail.com>
2022-06-26 15:59:20 +0700

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { PromoComponent } from './components/promo/promo.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    PromoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
