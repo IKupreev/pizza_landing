46bf46852b492066693f2050ea83b536eea0db6e
IKupreev <igor.kupreev88@gmail.com>
2022-08-10 11:37:03 +0700

    finish cart component

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -9,6 +9,7 @@ export class CartService {
 
   eats: Pizza[] = [];
   count = 0;
+  sum = 0;
   productCounter: BehaviorSubject<number> = new BehaviorSubject(0);
 
 
@@ -18,10 +19,10 @@ export class CartService {
     this.productCounter.next(this.count);
   }
 
-  // sumCart() {
-  //    this.count = this.eats.map(o => o.variants.map(v => v.price)).reduce((a,b) => a + b);
-  //    return this.count;
-  // }
+  sumCart() {
+     this.sum = this.eats.map(o => o.price).reduce((a,b) => a + b);
+     return this.sum;
+  }
 
 
   getItems() {
@@ -29,8 +30,10 @@ export class CartService {
   }
 
   clearCart() {
+    this.count = 0;
     this.eats = [];
-    this.productCounter.next(0);
+    this.productCounter.next(this.count);
     return this.eats;
+
   }
 }
